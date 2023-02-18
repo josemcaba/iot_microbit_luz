9438c39f7a96c40c21e09e5932b979c680fa50c7
josemcaba <96978309+josemcaba@users.noreply.github.com>
2023-02-18 03:15:43 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,30 @@
+ESP8266_IoT.iotSwitchEvent(ESP8266_IoT.KidsIotSwitchState.off, function () {
+    basic.showIcon(IconNames.Sad)
+})
+ESP8266_IoT.iotSwitchEvent(ESP8266_IoT.KidsIotSwitchState.on, function () {
+    basic.showIcon(IconNames.Happy)
+})
+ESP8266_IoT.initWIFI(SerialPin.P8, SerialPin.P12, BaudRate.BaudRate115200)
+basic.showIcon(IconNames.SmallDiamond)
+ESP8266_IoT.connectWifi("wiot", "a1b2c3d4")
+basic.showIcon(IconNames.Diamond)
+ESP8266_IoT.connectKidsiot("3a98OSfKko3Xge0G", "1")
+basic.showIcon(IconNames.Target)
+OLED.init(128, 64)
+basic.forever(function () {
+    OLED.clear()
+    OLED.writeString("Nivel de luz: ")
+    OLED.writeNumNewLine(Environment.ReadLightIntensity(AnalogPin.P2))
+    ESP8266_IoT.uploadKidsiot(Environment.ReadLightIntensity(AnalogPin.P2))
+    basic.pause(120000)
+})
+control.inBackground(function () {
+    while (true) {
+        if (ESP8266_IoT.kidsiotState(false)) {
+            basic.showIcon(IconNames.Diamond)
+            ESP8266_IoT.connectKidsiot("3a98OSfKko3Xge0G", "1")
+            basic.showIcon(IconNames.Target)
+        }
+        basic.pause(15000)
+    }
+})
